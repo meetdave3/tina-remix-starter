8129f97f456bd276fcc5fa3d59209d2e2ac73820
Meet Dave <meet.dave@outlook.com>
2023-10-20 12:44:32 +0200

    chore: add image

--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -4,15 +4,15 @@ export const Layout = (props) => {
   return (
     <div
       style={{
-        margin: '3rem',
+        margin: "3rem",
       }}
     >
       <header>
         <Link to="/">Home</Link>
-        {' | '}
+        {" | "}
         <Link to="/posts">Posts</Link>
       </header>
       <main>{props.children}</main>
     </div>
-  )
-}
+  );
+};
